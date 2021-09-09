35e5ea2140463ecc797006f7c6d4cf65c7c0030c
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-09-10 02:28:35 +0300

    add api fetch

---
+++ src/services/images-api.js
@@ -0,0 +1,17 @@
+function fetchImages(searchValue, page) {
+  return fetch(
+    `https://pixabay.com/api/?q=${searchValue}&page=${page}&key=22121707-46a45fd40074b90b571e26051&image_type=photo&orientation=horizontal&per_page=12`
+  ).then((response) => {
+    console.log(response);
+    if (response.ok) {
+      return response.json();
+    }
+
+    return Promise.reject(
+      new Error(`Sorry!We don't have images with this name`)
+    );
+  });
+}
+export const api = {
+  fetchImages,
+};
